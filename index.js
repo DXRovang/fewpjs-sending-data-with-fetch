7a84b346694490418c3ce397a51f6cc05c8c7dd7
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-22 14:31:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+let newData = {
+  name: name,
+  email: email
+};
+
+let configObj = {
+  method: "POST", 
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(newData)
+}
+
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", configObj)
+}
